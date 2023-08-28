ea68607cabe24a12de5f40f8c1164f6f4af98db6
chinmaya <chinmaya@users.noreply.github.com>
2023-08-28 19:08:28 +0530

    the windows setup / setpath file

---
+++ setpath.bat
@@ -0,0 +1,6 @@
+set PATH=%PATH%;"C:\Program Files\Sublime Text"
+set PATH=%PATH%;C:\Users\Maya\scoop\apps\nvm\1.1.11\nodejs\v18.16.1
+set PATH=%PATH%;C:\Users\Maya\scoop\apps\git\current\usr\bin
+set PATH=C:\Users\Maya\miniconda3;%PATH%;
+set PATH=C:\Users\Maya\miniconda3\condabin;%PATH%;
+set PATH=C:\Users\Maya\miniconda3\Scripts;%PATH%;
